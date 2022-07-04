35cf3c2c6177a45482f6bea009cf6c50ee2d6878
sanagi_nsh <nunua1172@gmail.com>
2022-07-04 19:26:08 +0900

    修正

--- DX12_Test/Matrix.cpp
+++ DX12_Test/Matrix.cpp
@@ -9,7 +9,7 @@ void Matrix::Initialize(ComPtr<ID3D12Device> device, int width, int height, D3D1
 
 	//égópÇ∑ÇÈçsóÒçÏê¨
 	_worldMat = XMMatrixRotationY(XM_PIDIV4);
-	XMFLOAT3 eye(0, 0, -);
+	XMFLOAT3 eye(0, 0, -2);
 	XMFLOAT3 target(0, 0, 0);
 	XMFLOAT3 up(0, 1, 0);
 	_viewMat = XMMatrixLookAtLH(XMLoadFloat3(&eye), XMLoadFloat3(&target), XMLoadFloat3(&up));
